c9a644ecaac4a158168b7ee1f36cfa7a38847e01
watanabeshunya <watana9748@gmail.com>
2021-02-18 22:32:29 +0900

    jsの骨組みを作成

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// 必要なHTML要素の取得
+const wrap = document.getElementById('wrap');
+const start = document.getElementById('start');
+
+const textLists = []; // 複数のテキストを格納する配列
+
+
+const createText = () => {}; // ランダムなテキストを画面に表示する
+
+const keyDown = e => {}; // キーイベント＆入力判定処理
+
+const rankCheck = rank => {}; // ランク判定とメッセージ生成処理
+
+const gameOver = id => {}; // ゲームの終了処理
+
+const timer = () => {}; // タイマー処理
+
+
+start.addEventListener('click', () => {}); // ゲームスタート時の処理
