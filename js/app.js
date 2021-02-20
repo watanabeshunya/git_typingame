639ba39bca32de753ecf39235f3873c1fc0839e5
watanabeshunya <watana9748@gmail.com>
2021-02-20 14:09:14 +0900

    テキストのリストを作成

--- js/app.js
+++ js/app.js
@@ -2,7 +2,21 @@
 const wrap = document.getElementById('wrap');
 const start = document.getElementById('start');
 
-const textLists = []; // 複数のテキストを格納する配列
+const textLists = [
+    'Hello World','This is my App','How are you?',
+  'Today is sunny','I love JavaScript!','Good morning',
+  'I am Japanese','Let it be','Samurai',
+  'Typing Game','Information Technology',
+  'I want to be a programmer','What day is today?',
+  'I want to build a web app','Nice to meet you',
+  'Chrome Firefox Edge Safari','machine learning',
+  'Brendan Eich','John Resig','React Vue Angular',
+  'Netscape Communications','undefined null NaN',
+  'Thank you very much','Google Apple Facebook Amazon',
+  'ECMAScript','console.log','for while if switch',
+  'var let const','Windows Mac Linux iOS Android',
+  'programming'
+]; // 複数のテキストを格納する配列
 
 
 const createText = () => {}; // ランダムなテキストを画面に表示する
